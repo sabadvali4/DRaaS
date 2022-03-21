45cf3f2db547b0c6d72f852b0f12965ddd7dc210
user.name "Oz Pozner <OzP@bynet.co.il>
2022-03-21 02:28:40 +0200

    getting ACI tenants

--- aci.py
+++ aci.py
@@ -24,24 +24,24 @@ def get_token(APIC):
   token = response['imdata'][0]['aaaLogin']['attributes']['token']
   return token
 
-def my_post(URL,payload,token = null):
+def my_post(URL,payload,token = None):
   url = URL
   data = payload
-  if (token == null):
+  if (token == None):
     headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
   else:
     headers = {'Content-type': 'application/json', 'Accept': 'application/json', "Cookie" : f"APIC-Cookie={token}"}  
   requests.packages.urllib3.disable_warnings()
   return requests.post(url, data=json.dumps(data), headers=headers, verify=False)
 
-def my_get(URL,token = null):
+def my_get(URL,token = None):
   url = URL
-  if (token == null):
+  if (token == None):
     headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
   else:
     headers = {'Content-type': 'application/json', 'Accept': 'application/json', "Cookie" : f"APIC-Cookie={token}"}  
   requests.packages.urllib3.disable_warnings()
-  return requests.post(url, headers=headers, verify=False)
+  return requests.get(url, headers=headers, verify=False)
   
   
 def main():
@@ -53,15 +53,22 @@ def main():
   """create tenant"""
   NAME = "Leumit"
   DN = f"tn-{NAME}"
-  URL = f"https://{APIC}/api/node/mo/uni/{DN}.json
-  payload = {"fvTenant":{"attributes":{"dn":"uni/{DN}","name":{NAME},"nameAlias":{NAME},"rn":{DN},"status":"created"},"children":[]}}
+  URL = f"https://{APIC}/api/node/mo/uni/{DN}.json"
+  payload = {"fvTenant":{"attributes":{"dn":f"uni/{DN}","name":NAME,"nameAlias":NAME,"rn":DN,"status":"created"},"children":[]}}
   response=my_post(URL,payload,token)
-  print("Create Tenant: \n" ,response)
+  print("Response code ["+str(response)+"] Create tenant : \n")
+  data = response.content
+  print(data)
   """ list all Tenants """
   url = f"https://{APIC}/api/node/class/fvTenant.json"
   payload={}
   response=my_get(URL,token)
-  print("tenant list: \n",response)
+  print("Response code ["+str(response)+"] tenant list: \n")
+  data = response.content
+  print(data)
+
+  
+  
   
 if __name__ == "__main__":
    main()
