bcf086e6a671847ae92cdcec238d12a74aa0d2d5
user.name "Oz Pozner <OzP@bynet.co.il>
2022-03-21 03:07:13 +0200

    getting ACI parsing json

---
+++ Readme.md
@@ -0,0 +1,39 @@
+**Windows**
+
+install python from cmd shell type:
+`python` and MS store shall popup, select python3 and click get/install.
+After python is install install pip: by typing in cmd shell 
+`python.exe -m pip install --upgrade pip`
+after pip is installed from command shell type: 
+
+`pip install -U python-dotenv requests configparser paramiko confparser`
+removed sys windows-curses
+
+you can navigate to control panel > System and Security > System > Advanced system Settings.
+Now in Advance System Setting click on Environment Variables.
+Here we can add new user variables and new system variables. We will add user variable by clicking New under user variables.
+
+In the new window you can add Variable name and Variable value and click ok.
+Now, click Ok on Environment Variables window to save changes.
+
+**Linux**
+
+Start by updating the package list using the following command: `sudo apt update`.
+Use the following command to install pip for Python 3: `sudo apt install python3-pip`. ...
+Once the installation is complete, verify the installation by checking the pip version: `pip3 --version`.
+`pip3 install -U python-dotenv requests configparser paramiko  confparser`
+removed sys curses
+
+nano ~/.bash_profile
+export USER="username"
+export PASSWORD="password"
+
+
+**Setup Paramters**
+
+cd ./config/
+cp/copy parameters.ini.example parameters.ini
+edit parameters.ini with users/password/IPs
+
+**Execute**
+from shell run `python main.py`
--- aci.py
+++ aci.py
@@ -1,7 +1,8 @@
-""" get Token """
+
 import requests
 import json
 
+""" get Token """
 def get_token(APIC):  
   url = f"https://{APIC}/api/aaaLogin.json"
 
@@ -42,7 +43,21 @@ def my_get(URL,token = None):
     headers = {'Content-type': 'application/json', 'Accept': 'application/json', "Cookie" : f"APIC-Cookie={token}"}  
   requests.packages.urllib3.disable_warnings()
   return requests.get(url, headers=headers, verify=False)
-  
+
+
+def printj(json_object):
+  if type(json_object) is list:
+    json_formatted_str = json.dumps(json_object[0], indent=4, sort_keys=True)
+  elif type(json_object) is dict:
+     json_formatted_str = json.dumps(json_object, indent=4 , sort_keys=True)
+  elif type(json_object) is str: 
+     json_formatted_str = json.dumps(json_object , indent=4, sort_keys=True) 
+  elif type(json_object) is bytes: 
+     json_encoded = json_object.decode("utf-8")
+     json_formatted_str = json.dumps(json_encoded, indent=4, sort_keys=True)   
+  else:
+     json_formatted_str = "Dont know how to print: "+str(type(json_object))    
+  print(json.loads(json_formatted_str))  
   
 def main():
   APIC = "10.10.20.14" 
@@ -58,17 +73,14 @@ def main():
   response=my_post(URL,payload,token)
   print("Response code ["+str(response)+"] Create tenant : \n")
   data = response.content
-  print(data)
+  printj(data)
   """ list all Tenants """
   url = f"https://{APIC}/api/node/class/fvTenant.json"
   payload={}
   response=my_get(URL,token)
   print("Response code ["+str(response)+"] tenant list: \n")
-  data = response.content
-  print(data)
+  printj(response.content)
+  
 
-  
-  
-  
 if __name__ == "__main__":
    main()
