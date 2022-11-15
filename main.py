a715132ae10e49c6f95957eb49314864976c2410
Dana Israeli <danai@bynet.co.il>
2022-11-15 11:21:58 +0200

    fix url settings

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 NULL = 0
 from collections.abc import Mapping
 import settings
-import requests
+# import requests
 import json
 # parameters from ENV
 from dotenv import load_dotenv   #for python-dotenv method
@@ -22,6 +22,8 @@ import paramiko
 import sys
 import confparser
 from functions import run
+from functions import today
+from functions import get_commands_from_snow
 
 
 load_dotenv()
@@ -71,15 +73,17 @@ else:
     switches_username = os.environ.get('switches_username')
     switches_password = os.environ.get('switches_password')
 
-if int(debug_level) > 8:
+if int(debug_level) > 2:
     print("Today:", today(), "\n")
     print("url: ", url, "\n")
     print("username: ", username, "\n")
     print("password: ", password, "\n")
     print("Switches IP", ips, "\n")
 
+#get_commands_from_snow(hostname='YanirServer',ip='none', url)
 
 if __name__ == "__main__":
     settings.init()
     run()
+    #get_commands_from_snow(hostname='YanirServer', url)
 
