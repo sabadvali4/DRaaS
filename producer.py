047e9aad697547a60f6be188b4db44225e35e8d1
Maya <maya@devops-x.net>
2023-11-22 15:22:18 +0200

    edit the send_health_monitor to send the information in the right form

--- producer.py
+++ producer.py
@@ -1,5 +1,5 @@
 import redis, requests
-import re, json
+import re, json, math
 import time; from time import * 
 import requests; import re; import json; import logging
 from datetime import datetime
@@ -60,14 +60,14 @@ def send_status_update(ID, STATUS, OUTPUT):
         logger.error('Error in send_status_update: %s', str(e))
 
 
-def send_health_monitoring_update(mid_name, items_in_queue, items_in_process, timestamp):
+def send_health_monitoring_update(mid_name, items_in_queue, items_in_process, Timestamp):
     try:
         payload = json.dumps(
             {
                 "mid_name": mid_name,
                 "items_in_queue": items_in_queue,
                 "items_in_process": items_in_process,
-                "timestamp": timestamp
+                "timestamp": Timestamp
             })
         print(payload)
         answer = requests.post(update_status_url, data=payload,
@@ -114,6 +114,6 @@ if __name__ == "__main__":
         tasks_len = len(get_requests())  # Use a different variable name
         redis_queue_push(get_requests())
         # Format timestamp as HH:MM:SS
-        timestamp = datetime.now().strftime('%H:%M:%S')
-        send_health_monitoring_update(settings.mid_server, redis_server.llen(queue_name), (tasks_len - redis_server.llen(queue_name)), timestamp)
+        Timestamp = datetime.now().strftime('%H:%M:%S')
+        send_health_monitoring_update(settings.mid_server, redis_server.llen(queue_name), abs(tasks_len), Timestamp)
         sleep(10)
