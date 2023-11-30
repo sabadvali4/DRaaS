243a6ffcf6bde02870ded10bf07d0485ad1f7bcf
Maya <maya@devops-x.net>
2023-11-30 09:44:37 +0200

    updated the sh file to backup ini

--- update_script.sh
+++ update_script.sh
@@ -6,8 +6,15 @@ project_dir="$(pwd)"
 # Log file path
 log_file="$project_dir/update_script.log"
 
-# Get the current local branch
-current_branch=$(git rev-parse --abbrev-ref HEAD)
+# Find the configuration file under the 'config' directory
+config_dir="$project_dir/config"
+ini_file="$(find "$config_dir" -maxdepth 1 -type f -iname "*.ini" -print -quit)"
+
+# Check if the parameters.ini file was found
+if [ -z "$ini_file" ]; then
+    echo "Error: No configuration file (*.ini) found in the 'config' directory. Please check your repository."
+    exit 1
+fi
 
 # Discard local changes and reset to the remote branch
 git fetch origin "$current_branch"
@@ -27,6 +34,7 @@ consumer_status=$(sudo systemctl is-active consumer.service)
 # Print the status message
 if [ "$producer_status" = "active" ] && [ "$consumer_status" = "active" ]; then
     echo "All services are up."
+    echo "MID Server: $mid_server"
 else
     echo "Something went wrong. Check the status of your services. See the log file for details: $log_file"
 fi
