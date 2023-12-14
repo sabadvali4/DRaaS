0bcd521aab7b7c5a3a883209725f06284efb427c
Maya <maya@devops-x.net>
2023-12-14 14:54:13 +0200

    edited the script with log file and prints to it

--- script.sh
+++ script.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-update_file="/opt/servicenow/mid/agent/export/update_xxxx.zip"
-drass_destination="/path/to/drass"  ##where??
+DATE=$(date "+%Y%m%d%H%M")
+# Log file path
+log_file="/var/log/script.log"
 
+update_file="/opt/servicenow/mid/agent/export/update_xxxx.zip"
+drass_destination= $(pwd)
+
+echo "Started sync at ${DATE}"  >> "$log_file"
 # Check if the update file exists
 while [ ! -e "$update_file" ]; do
-    echo "Waiting for update file..."
+    echo "Waiting for update file..." >> "$log_file"
     sleep 5
 done
 
 # Extract the contents of the update file.
 temp_extracted_dir=$(mktemp -d)
 unzip -q "$update_file" -d "$temp_extracted_dir"
+echo "extracted the folder to "$temp_extracted_dir". " >> "$log_file"
 
 # Update specific files in DRaaS folder
 for file in "$temp_extracted_dir"*; do
@@ -21,13 +27,14 @@ for file in "$temp_extracted_dir"*; do
     if [[ "$filename" != *.config && "$filename" != *.ini ]]; then
         # Update the file in the destination folder
         cp "$file" "$drass_destination/$filename"
+        echo "copied the new file: "$file"." >> "$log_file"
     fi
 done
 
 # Clean up temporary directory
 rm -r "$temp_extracted_dir"
 
-echo "Update applied successfully."
+echo "Update applied successfully." >> "$log_file"
 
 
 
