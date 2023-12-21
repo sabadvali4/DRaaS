4a44e3c232887e48686b8261806f78f30d095155
Maya <maya@devops-x.net>
2023-12-21 09:49:05 +0200

    added function to get route info from gaia

--- gaia_ssh_connect.py
+++ gaia_ssh_connect.py
@@ -1,7 +1,6 @@
 import paramiko
 import time, re
 import json
-
 class SSHConnection:
     shell = None
     client = None
@@ -48,7 +47,6 @@ def get_interface_info(ip, user, password):
     connection.close_connection()
     return interface_info
 
-
 def get_gaia_interface_info(ip, user, password):
     #Login by ssh
     connection = SSHConnection(ip, user, password)
@@ -62,6 +60,7 @@ def get_gaia_interface_info(ip, user, password):
     parsed_data = parse_gaia_output(output_str)
     json_data = json.dumps(parsed_data, indent=4)
     
+    connection.close_connection()
     return json_data
 
 def parse_gaia_output(output):
@@ -84,6 +83,22 @@ def parse_gaia_output(output):
                 interfaces[current_interface][key] = value
     return interfaces
 
+def get_gaia_route_info(ip, user, password):
+    connection = SSHConnection(ip, user, password)
+    time.sleep(1)
+    # Send command to get route information
+    output = connection.exec_command('show route')
+    # Join the output to form a string
+    output_str = '\n'.join(output)
+
+    # Parse the output to JSON format
+    #parsed_data = parse_gaia_route_output(output_str)
+    json_data = json.dumps(output_str, indent=4)
+    
+    connection.close_connection()
+    return json_data
+
+
 if __name__ == "__main__":
     gaia_ip = "10.169.32.178"
     gaia_username = "admin"
@@ -91,3 +106,6 @@ if __name__ == "__main__":
 
     gaia_interface_info = get_gaia_interface_info(gaia_ip, gaia_username, gaia_password)
     print(gaia_interface_info)
+
+    gaia_route_info = get_gaia_route_info(gaia_ip, gaia_username, gaia_password)
+    print(gaia_route_info)
